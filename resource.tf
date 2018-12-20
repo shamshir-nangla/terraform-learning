3566742da7a8208d61db1d0fbdaa18737222c396
shamshir-nangla <shamshir.nangla@sage.com>
2018-12-20 12:12:21 +0000

    second learning sess

--- resource.tf
+++ resource.tf
@@ -3,6 +3,31 @@ resource "aws_vpc" "environment-example-two" {
   enable_dns_hostnames = true
   enable_dns_support = true
   tags {
-    Name = "terraform-aws-vpc-example2-two"
+    Name = "terraform-aws-vpc-example-two"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-west-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-west-1c"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
   }
 }
